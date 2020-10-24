dd5b79bf2b62eb858f1e5ab83774764f325f3174
cagloria <33134245+cagloria@users.noreply.github.com>
2020-10-24 14:03:53 -0700

    Added event listener for nav overlay

--- src/main.js
+++ src/main.js
@@ -1,5 +1,26 @@
+function toggleNav() {
+    const $header = $(".header");
+    const $navOverlay = $(".nav-overlay");
+    const headerNavActiveClass = "header--nav-active";
+    const $navButton = $("#nav__button");
+
+    $header.toggleClass(headerNavActiveClass);
+
+    if ($header.hasClass(headerNavActiveClass)) {
+        $navButton.attr("aria-expanded", "true");
+        $navOverlay.on("click", function () {
+            toggleNav();
+        });
+    } else {
+        $navButton.attr("aria-expanded", "false");
+        $navOverlay.off();
+    }
+}
+
 window.onload = function () {
-    $("#nav__button").on("click", function () {
-        $(".header").toggleClass("header--nav-active")
+    const $navButton = $("#nav__button");
+
+    $navButton.on("click", function () {
+        toggleNav();
     });
 };
