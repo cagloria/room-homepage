d92420ef0ffa531916aa69c0aea436f7bc9f7f7c
cagloria <33134245+cagloria@users.noreply.github.com>
2020-10-24 14:09:06 -0700

    Improved accessibility for nav

--- src/main.js
+++ src/main.js
@@ -1,18 +1,21 @@
 function toggleNav() {
     const $header = $(".header");
     const $navOverlay = $(".nav-overlay");
-    const headerNavActiveClass = "header--nav-active";
     const $navButton = $("#nav__button");
+    const $navLink = $(".nav a");
+    const headerNavActiveClass = "header--nav-active";
 
     $header.toggleClass(headerNavActiveClass);
 
     if ($header.hasClass(headerNavActiveClass)) {
         $navButton.attr("aria-expanded", "true");
+        $navLink.attr("tabindex", "0");
         $navOverlay.on("click", function () {
             toggleNav();
         });
     } else {
         $navButton.attr("aria-expanded", "false");
+        $navLink.attr("tabindex", "-1");
         $navOverlay.off();
     }
 }
